3561df371e082f5d62a8debd963ca7ce2d34a967
ahmedayman10 <ahmedayman44477@gmail.com>
2022-05-26 20:49:46 +0200

    last commit

--- controllers/cart.js
+++ controllers/cart.js
@@ -2,7 +2,8 @@
 const {Cart}=require("../models/cart");
 const {Book}=require("../models/book");
 exports.cart=async(req,res)=>{
-    let cart=await Cart.findOne({user:"628f9d182c2a0becc5dd7f2d"}).populate("items");
+    session =req.session.userid 
+    let cart=await Cart.findOne({user:session}).populate("items");
    if(cart){
     return   res.render("kart",{cart,bell: cart.items.length});
    }
@@ -10,16 +11,17 @@ exports.cart=async(req,res)=>{
 }
 
 exports.addToCart=async(req,res)=>{
-    
+    session =req.session.userid 
+
     const {params:{id}}=req;
-    const cart=await Cart.findOne({user:"628f9d182c2a0becc5dd7f2d"});
+    const cart=await Cart.findOne({user:session});
     const book=await Book.findOne({_id:id});
     if(cart){
-        await Cart.updateOne({user:"628f9d182c2a0becc5dd7f2d"},{$push:{"items":book._id},$inc:{totalPrice:+book.price}});
+        await Cart.updateOne({user:session},{$push:{"items":book._id},$inc:{totalPrice:+book.price}});
     }
     else{
        
-        await new Cart({user:"628f9d182c2a0becc5dd7f2d",items:[book._id],totalPrice:book.price}).save();
+        await new Cart({user:session,items:[book._id],totalPrice:book.price}).save();
         
     }
 
@@ -27,7 +29,8 @@ exports.addToCart=async(req,res)=>{
 }
 
 exports.emptyCart=async(req,res)=>{
-  await  Cart.findOneAndDelete({user:"628f9d182c2a0becc5dd7f2d"});
+session =req.session.userid 
+  await  Cart.findOneAndDelete({user:session});
   res.redirect("/kitap")
 }
 
